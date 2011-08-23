b532eb7e3095455f98bd6e62e1c0a557a6dad8bb
Matt Atkins <matt@yoomee.com>
2011-08-23 17:19:18 +0100

    Subnav fix. Feed back form, nearly there.

--- client/app/controllers/sections_controller.rb
+++ client/app/controllers/sections_controller.rb
@@ -1,7 +1,7 @@
 SectionsController.class_eval do
 
   def show_with_client
-    if @section.slug.match(/_products$/)
+    if @section.slug.try(:match, /_products$/)
       @pages_sections = @section.pages + @section.children
       @pages_sections = @pages_sections.sort_by(&:weight)
       @departments = @section.pages.tag_counts.sort
