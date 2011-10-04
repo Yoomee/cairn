c9b73de10adb82754cf831b64b7f282dcff78a33
Ian Mooney <ian@yoomee.com>
2011-10-04 15:37:08 +0100

    Don't show hidden sections in nav.

--- client/app/controllers/sections_controller.rb
+++ client/app/controllers/sections_controller.rb
@@ -2,12 +2,12 @@ SectionsController.class_eval do
 
   def show_with_client
     if @section.slug.try(:match, /_products$/)
-      @pages_sections = @section.pages + @section.children
+      @pages_sections = @section.pages + @section.children.not_hidden
       @pages_sections = @pages_sections.sort_by(&:weight)
       @departments = @section.pages.tag_counts.sort
       render :action => :products
     elsif @section.view == "normal"
-      @pages_sections = @section.pages + @section.children
+      @pages_sections = @section.pages + @section.children.not_hidden
       @pages_sections = @pages_sections.sort_by(&:weight)
       render
     else
