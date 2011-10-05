749b58ea555512770fa276cbed68d563b3d16296
Ian Mooney <ian@yoomee.com>
2011-10-05 12:39:37 +0100

    Added edit slideshow link on homepage slideshow.

--- client/app/controllers/slideshows_controller.rb
+++ client/app/controllers/slideshows_controller.rb
@@ -3,7 +3,7 @@ SlideshowsController.class_eval do
   def update
     if @slideshow.update_attributes(params[:slideshow])
       flash[:notice] = "Successfully updated slideshow."
-      redirect_to admin_path
+      redirect_to home_path
     else
       render :action => 'edit'
     end
